2f8c291c598faedba87b0d13d423243565b5d842
Uqyanzie <uqyanzie@gmail.com>
2022-08-29 14:23:21 +0700

    added description in data

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'A pair of wet and stinky socks'
         }
     }
 })
