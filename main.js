aa4322051021f4a3453f66ccc9d29d79d5877e6d
Uqyanzie <uqyanzie@gmail.com>
2022-08-29 14:31:33 +0700

    solved

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'A pair of wet and stinky socks'
+            description: 'A pair of wet and stinky socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://github.com/uqyanzie/Intro-to-Vue-3'
         }
     }
 })
