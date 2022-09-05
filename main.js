fec8def06d4365a4ce3f78e81dee9ea7bdf1199f
Uqyanzie <uqyanzie@gmail.com>
2022-09-05 21:16:38 +0700

    SOLVED

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 100 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            onsale: true,
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product + (this.onSale ? ' (on Sale)' : ' (not on Sale)')
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
+
 })
